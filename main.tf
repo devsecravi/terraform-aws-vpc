966365a2bf2b74d31069976ab74646beb71ccc97
unknown <dasariravid145@gmail.com>
2026-05-18 12:10:54 +0530

    action

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_subnet" "public" {
     tags =  merge(
           local.common_tags,
           {
-             Name = "${var.project}-{var.environment}-public-${local.az_names[count.index]}"
+             Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
           },
           var.public_subnet_tags
     )
