817ec496d7c1769a9d9420a2b1029f8cf0e232aa
unknown <dasariravid145@gmail.com>
2026-05-18 11:12:29 +0530

    action

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
   enable_dns_hostnames= true
 
   tags = local.vpc2_final_tags
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags =local.igw_final_tags
 }
 
